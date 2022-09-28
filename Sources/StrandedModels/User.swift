2bca9ace54ce3f4f4ede05b7d700b001320f5f80
Gutierrez, Idelfonso <idelfonsog2@gmail.com>
2022-09-28 16:11:15 -0500

    #2 chore: random

--- Sources/StrandedModels/User.swift
+++ Sources/StrandedModels/User.swift
@@ -11,7 +11,7 @@ import FluentKit
 final public class User: Model {
     public static let schema: String = "users"
     
-    @ID(custom: "id", generatedBy: .database)
+    @ID(custom: "id", generatedBy: .random)
     public var id: UUID?
     
     @OptionalChild(for: \Subscription.$user)
